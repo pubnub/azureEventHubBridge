f55fe62a73ee37bd9be3c35db47f25874563509b
gcohen <geremy@pubnub.com>
2016-09-26 16:34:17 -0700

    First commit

---
+++ app.js
@@ -0,0 +1,70 @@
+'use strict';
+
+var pubnub = require("pubnub")({
+    ssl: true,  // <- enable TLS Tunneling over TCP
+    publish_key: "demo-36",
+    subscribe_key: "demo-36"
+});
+
+var EventHubClient = require('azure-event-hubs').Client;
+var Promise = require('bluebird');
+
+
+
+var PNPublish = function(relayMessage) {
+    console.log("LOG: " + JSON.stringify(relayMessage));
+    pubnub.publish({
+        channel: "bot-relay",
+        message: relayMessage.body
+    });
+}
+
+var connectionString = 'Endpoint=sb://autonubeventhub.servicebus.windows.net/;SharedAccessKeyName=infromsubscriberhub;SharedAccessKey=533HJhCxZIynOV1xbQKBWgilDQ4euKRSUxWsbZBG1v4=;EntityPath=infrompnsubscriber';
+var eventHubPath = '';
+
+var sendEvent = function (eventBody) {
+    return function (sender) {
+        console.log('Sending Event: ' + eventBody);
+        return sender.send(eventBody);
+    };
+};
+
+var printError = function (err) {
+    console.error(err.message);
+};
+
+var printEvent = function (ehEvent) {
+    PNPublish(ehEvent);
+
+    console.log('Event Received: ');
+    console.log(JSON.stringify(ehEvent.body));
+    console.log('');
+};
+
+var client = EventHubClient.fromConnectionString(connectionString, eventHubPath);
+var receiveAfterTime = Date.now() - 5000;
+
+client.open()
+    .then(client.getPartitionIds.bind(client))
+    .then(function (partitionIds) {
+        return Promise.map(partitionIds, function (partitionId) {
+            return client.createReceiver('$Default', partitionId, { 'startAfterTime' : receiveAfterTime}).then(function (receiver) {
+                receiver.on('errorReceived', printError);
+                receiver.on('message', printEvent);
+            });
+        });
+    })
+    //.then(function () {
+    //    return client.createSender();
+    //})
+    //.then(sendEvent('foo'))
+    .catch(printError);
+
+
+pubnub.subscribe({
+    channel: "bot",
+    message: function (message) {
+        // console.log(" > ", message);
+        client.createSender().then(sendEvent(message));
+    }
+});
