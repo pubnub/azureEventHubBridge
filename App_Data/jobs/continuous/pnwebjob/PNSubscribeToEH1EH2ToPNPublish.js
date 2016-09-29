b9d793fcb0c256fa99428a24052c854a33c4804e
gcohen <geremy@pubnub.com>
2016-09-29 13:58:37 -0700

    Refactored code to put all vars at top.  Will make parameterizing the vars in the script easier.

--- App_Data/jobs/continuous/pnwebjob/PNSubscribeToEH1EH2ToPNPublish.js
+++ App_Data/jobs/continuous/pnwebjob/PNSubscribeToEH1EH2ToPNPublish.js
@@ -3,18 +3,30 @@
 
 'use strict';
 
+// PN Vars
+
 var PNSubChannel = "bot_object";
 var PNPubChannel = "bot-relay";
+var PNPublishKey = "demo-36";
+var PNSubscribeKey = "demo-36";
+
+// Azure Vars
+
+var EHInConnectionString  = 'Endpoint=sb://autonubeventhub.servicebus.windows.net/;SharedAccessKeyName=infromsubscriberhub;SharedAccessKey=533HJhCxZIynOV1xbQKBWgilDQ4euKRSUxWsbZBG1v4=;EntityPath=infrompnsubscriber';
+var EHOutConnectionString = 'Endpoint=sb://autonubeventhub.servicebus.windows.net/;SharedAccessKeyName=outtopublisherhub;SharedAccessKey=GjfSXpxyIvVWzB6+DEZj9amxBIf1QV9XfyRtaujbTzo=;EntityPath=outtopnpublisher'
+
+// Begin Code
 
 var pubnub = require("pubnub")({
-    ssl: true,  // <- enable TLS Tunneling over TCP
-    publish_key: "demo-36",
-    subscribe_key: "demo-36"
+    ssl: true,
+    publish_key: PNPublishKey,
+    subscribe_key: PNSubscribeKey
 });
 
 var PNPublish = function(ehEvent) {
     console.log('Event Received from EHOutClient, Publishing via PubNub: ');
     console.log(JSON.stringify(ehEvent.body));
+    console.log("");
 
     pubnub.publish({
         channel: PNPubChannel,
@@ -27,8 +39,6 @@ var receiveAfterTime = Date.now() - 5000;
 var EventHubClient = require('azure-event-hubs').Client;
 var Promise = require('bluebird');
 
-var EHInConnectionString  = 'Endpoint=sb://autonubeventhub.servicebus.windows.net/;SharedAccessKeyName=infromsubscriberhub;SharedAccessKey=533HJhCxZIynOV1xbQKBWgilDQ4euKRSUxWsbZBG1v4=;EntityPath=infrompnsubscriber';
-var EHOutConnectionString = 'Endpoint=sb://autonubeventhub.servicebus.windows.net/;SharedAccessKeyName=outtopublisherhub;SharedAccessKey=GjfSXpxyIvVWzB6+DEZj9amxBIf1QV9XfyRtaujbTzo=;EntityPath=outtopnpublisher'
 
 var printError = function (err) {
     console.error(err.message);
