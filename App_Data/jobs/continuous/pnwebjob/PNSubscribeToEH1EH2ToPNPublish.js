5a1de4149c13d67b5b57974b6011f8ce82ec3829
gcohen <geremy@pubnub.com>
2016-09-29 17:04:12 -0700

    Adding console logs to troubleshoot env var

--- App_Data/jobs/continuous/pnwebjob/PNSubscribeToEH1EH2ToPNPublish.js
+++ App_Data/jobs/continuous/pnwebjob/PNSubscribeToEH1EH2ToPNPublish.js
@@ -5,15 +5,15 @@
 
 // PN Vars
 
-var PNSubChannel = process.env.PNSubChannel;    // "bot_object";
-var PNPubChannel = process.env.PNPubChannel;    // "bot-relay";
-var PNPublishKey = process.env.PNPublishKey;    // "demo-36";
-var PNSubscribeKey = process.env.PNSubscribeKey;  // "demo-36";
+var PNSubChannel = process.env['PNSubChannel'];    // "bot_object";
+var PNPubChannel = process.env['PNPubChannel'];    // "bot-relay";
+var PNPublishKey = process.env['PNPublishKey'];    // "demo-36";
+var PNSubscribeKey = process.env['PNSubscribeKey'];  // "demo-36";
 
 // Azure Vars
 
-var EHInConnectionString  = process.env.EHInConnectionString; // 'Endpoint=sb://autonubeventhub.servicebus.windows.net/;SharedAccessKeyName=infromsubscriberhub;SharedAccessKey=533HJhCxZIynOV1xbQKBWgilDQ4euKRSUxWsbZBG1v4=;EntityPath=infrompnsubscriber';
-var EHOutConnectionString = process.env.EHOutConnectionString; // 'Endpoint=sb://autonubeventhub.servicebus.windows.net/;SharedAccessKeyName=outtopublisherhub;SharedAccessKey=GjfSXpxyIvVWzB6+DEZj9amxBIf1QV9XfyRtaujbTzo=;EntityPath=outtopnpublisher'
+var EHInConnectionString  = process.env['EHInConnectionString']; // 'Endpoint=sb://autonubeventhub.servicebus.windows.net/;SharedAccessKeyName=infromsubscriberhub;SharedAccessKey=533HJhCxZIynOV1xbQKBWgilDQ4euKRSUxWsbZBG1v4=;EntityPath=infrompnsubscriber';
+var EHOutConnectionString = process.env['EHOutConnectionString']; // 'Endpoint=sb://autonubeventhub.servicebus.windows.net/;SharedAccessKeyName=outtopublisherhub;SharedAccessKey=GjfSXpxyIvVWzB6+DEZj9amxBIf1QV9XfyRtaujbTzo=;EntityPath=outtopnpublisher'
 
 console.log(PNSubChannel);
 console.log(PNPubChannel);
