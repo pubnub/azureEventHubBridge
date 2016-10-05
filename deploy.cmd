f9cd5d4e035d4eb27e68108a47e98f0d817cb8bd
gcohen <geremy@pubnub.com>
2016-10-05 14:53:00 -0700

    Try again with npm install instead of tar

--- deploy.cmd
+++ deploy.cmd
@@ -64,11 +64,11 @@ IF !ERRORLEVEL! NEQ 0 goto error
 :: 2. Install npm packages
 IF EXIST "%DEPLOYMENT_TARGET%\App_Data\jobs\continuous\pnwebjob\package.json" (
   pushd %DEPLOYMENT_TARGET%\App_Data\jobs\continuous\pnwebjob
-  :: echo Starting npm install
-  :: call npm install
-  call tar xvfz node_modules.tgz
-  :: IF !ERRORLEVEL! NEQ 0 goto error
-  :: popd
+  echo Starting npm install
+  call npm install
+  :: call tar xvfz node_modules.tgz
+  IF !ERRORLEVEL! NEQ 0 goto error
+  popd
 
 )
 
