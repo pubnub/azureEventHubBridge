17eccf586f02a2c4a39ca2d78ce6a668cd69912d
gcohen <geremy@pubnub.com>
2016-12-08 13:10:33 -0800

    Cleaned up provisioning script a bit more.

--- monitoring/inputEHMonitor.js
+++ monitoring/inputEHMonitor.js
@@ -7,6 +7,8 @@ var Promise = require('bluebird');
 
 // The Event Hubs SDK can also be used with an Azure IoT Hub connection string.
 // In that case, the eventHubPath variable is not used and can be left undefined.
+
+
 var connectionString = 'Endpoint=sb://pn-eventhub-1fba54e9.servicebus.windows.net/;SharedAccessKeyName=infromsubscriberhub;SharedAccessKey=WxRcwPpzTU77tFY8E/1RZD2K6fhmkhsx5uHFK8zgU4o=;EntityPath=infromsubscriberhub';
 
 var sendEvent = function (eventBody) {
